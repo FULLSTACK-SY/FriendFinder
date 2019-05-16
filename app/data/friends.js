f3129f9cbb93e0df6419b9397875665f843b525e
TLI - Sushmita (SY) <sushmita@tli.com>
2019-05-15 23:42:17 -0400

    basic prog ready

---
+++ app/data/friends.js
@@ -0,0 +1,29 @@
+var friends = [
+	{
+		name: "Maria Mist",
+		photo: "https://image.shutterstock.com/image-photo/brunette-girl-long-shiny-wavy-450w-639921919.jpg",
+		scores: [5, 1, 2, 5, 1, 2, 5, 1, 1, 1]
+	},
+	{
+		name: "Arpan Mane",
+		photo: "https://cdn.pixabay.com/photo/2017/04/01/21/06/portrait-2194457_1280.jpg",
+		scores: [2, 5, 4, 1, 4, 3, 3, 2, 3, 3]
+	},
+	{
+		name: "Sara Pare",
+		photo: "https://cdn.pixabay.com/photo/2017/06/09/17/11/model-2387582_1280.jpg",
+		scores: [1, 1, 3, 3, 3, 5, 1, 2, 3, 4]
+	},
+	{
+		name: "Sonia Garg",
+		photo: "https://cdn.pixabay.com/photo/2016/03/23/08/34/beautiful-1274360_1280.jpg",
+		scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
+	},
+	{
+		name: "Frank Lee",
+		photo: "https://www.humanrightsatsea.org/wp-content/uploads/2016/01/Mauricio-Lazala.jpg",
+		scores: [2, 4, 4, 4, 2, 4, 2, 1, 1, 3]
+	}
+];
+
+module.exports = friends;
---
+++ server.js
@@ -0,0 +1,35 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var path = require("path");
+var bodyParser = require('body-parser');
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// BodyParser makes it easy for our server to interpret data sent to it.
+// The code below is pretty standard.
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({type:'application/vnd.api+json'}));
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// ROUTER
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or
+// request data from various URLs.
+// API and HTML routes
+require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
