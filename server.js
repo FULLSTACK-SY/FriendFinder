1558056ee7229c2b80409114571436abee70b142
TLI - Sushmita (SY) <sushmita@tli.com>
2019-05-19 18:38:39 -0400

    try access of css

--- server.js
+++ server.js
@@ -8,14 +8,6 @@ var path = require("path");
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-// BodyParser makes it easy for our server to interpret data sent to it.
-// The code below is pretty standard.
-var bodyParser = require('body-parser');
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.text());
-app.use(bodyParser.json({type:'application/vnd.api+json'}));
-
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
